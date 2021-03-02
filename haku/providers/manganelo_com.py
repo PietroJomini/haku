ed22383ff003f4e95f4909d2e15d47cc15744545
Pietro Jomini <pietro.jomini@protonmail.ch>
2021-03-03 00:11:05 +0100

    postponed manganelo testing provider, needs cloudfare bypass

--- haku/providers/manganelo_com.py
+++ haku/providers/manganelo_com.py
@@ -1,41 +1,46 @@
 from haku.provider import Provider
+from haku.meta import Chapter, Page
+from typing import List
+import aiohttp
+import re
+
+
+# TODO(me) switch images server
 
 
 class ManganeloCom(Provider):
     """Manganelo.com provider"""
 
     name = 'manganelo.com'
-    pattern = r'^https://mangarock.to'
+    pattern = r'^https://manganelo.com'
+    enabled = False
 
     async def fetch_cover_url(self, session: aiohttp.ClientSession, url: str):
         page = await self.helpers.scrape_and_cook(session, url)
-        thumb = page.select('div.thumb div')[0]['style']
-        meta = re.search(r'background-image: url\(\'(.*)\'\);', thumb)
-        return meta.group(1)
+        return page.select('span.info-image img')[0]['src']
 
     async def fetch_title(self, session: aiohttp.ClientSession, url: str):
         page = await self.helpers.scrape_and_cook(session, url)
-        return page.select('div.info h1')[0].text
+        return page.select('div.story-info-right h1')[0].text
 
     async def fetch_chapters(self, session: aiohttp.ClientSession, url: str) -> List[Chapter]:
         page = await self.helpers.scrape_and_cook(session, url)
 
         chapters = []
-        for chapter in page.select('div.all-chapers tbody a'):
-            meta = re.search(r'Vol.(\d*) *#(.*): *(.*)', chapter.text)
+        for chapter in page.select('a.chapter-name'):
+            meta = re.search(r'Vol.(\d*) Chapter (.*): *(.*)', chapter.text)
 
             volume = meta.group(1) if meta else None
             index = meta.group(2) if meta else ''
             title = meta.group(3) if meta else ''
             url = chapter['href']
 
-            if index != '' and title != '':
-                chapters.append(Chapter(
-                    volume=volume,
-                    index=index,
-                    title=title,
-                    url=url
-                ))
+            chapters.append(Chapter(
+                volume=volume,
+                index=index,
+                title=title,
+                url=url
+            ))
 
         return chapters
 
@@ -43,11 +48,11 @@ class ManganeloCom(Provider):
         page = await self.helpers.scrape_and_cook(session, chapter.url)
 
         pages = []
-        meta = re.search(r'var mangaData = (.*?);', str(page))
-        for index, page in enumerate(json.loads(meta.group(1))):
+        for image in page.select('div.container-chapter-reader img'):
+            url = image['src']
             pages.append(Page(
-                url=page['url'],
-                index=index
+                url=url,
+                index=int(re.search(r'.*\/(\d+)\..*', url).group(1))
             ))
 
         return pages
